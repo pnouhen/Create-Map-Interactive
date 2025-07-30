6a95152d543fa8471f4072f20efd618ff54101d9
PierreNouhen <pierre.nouhen@orange.fr>
2025-07-30 16:46:54 +0200

    Suppression du loader pour tester les perf en mobile

--- src/loader/generateLoader.js
+++ src/loader/generateLoader.js
@@ -1,23 +1,23 @@
 import { generateMap } from "../maps/generateMap";
 import {storeRnf} from "../datas/storeRnf"
 
-// Wait until the storeRnf array is not empty
-const waitForStore = new Promise((resolve) => {
-  const interval = setInterval(() => {
-    // Check if storeRnf has data
-    if (storeRnf.length > 0) {
-      clearInterval(interval);
-      resolve();
-    }
-  }, 100); 
-});
+// // Wait until the storeRnf array is not empty
+// const waitForStore = new Promise((resolve) => {
+//   const interval = setInterval(() => {
+//     // Check if storeRnf has data
+//     if (storeRnf.length > 0) {
+//       clearInterval(interval);
+//       resolve();
+//     }
+//   }, 100); 
+// });
 
-// Wait for both the map and the store to be ready
-Promise.all([generateMap(), waitForStore]).then(() => {
-  loader.classList.add("opacity-0");
-  loader.classList.remove("opacity-100");
+// // Wait for both the map and the store to be ready
+// Promise.all([generateMap(), waitForStore]).then(() => {
+//   loader.classList.add("opacity-0");
+//   loader.classList.remove("opacity-100");
 
-  setTimeout(() => {
-    loader.classList.add("hidden");
-  }, 500);
-});
+//   setTimeout(() => {
+//     loader.classList.add("hidden");
+//   }, 500);
+// });
