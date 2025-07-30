a8a27c680377bc13aef37f966e86691ca6a3947e
PierreNouhen <pierre.nouhen@orange.fr>
2025-07-30 16:51:16 +0200

    Modification du loader

--- src/loader/generateLoader.js
+++ src/loader/generateLoader.js
@@ -1,23 +1,30 @@
 import { generateMap } from "../maps/generateMap";
-import {storeRnf} from "../datas/storeRnf"
+import { storeRnf } from "../datas/storeRnf";
 
-// // Wait until the storeRnf array is not empty
-// const waitForStore = new Promise((resolve) => {
-//   const interval = setInterval(() => {
-//     // Check if storeRnf has data
-//     if (storeRnf.length > 0) {
-//       clearInterval(interval);
-//       resolve();
-//     }
-//   }, 100); 
-// });
+// Lancer la génération de la carte tout de suite
+const mapReady = generateMap();
 
-// // Wait for both the map and the store to be ready
-// Promise.all([generateMap(), waitForStore]).then(() => {
-//   loader.classList.add("opacity-0");
-//   loader.classList.remove("opacity-100");
+// Attendre que le store soit prêt (vérifie toutes les 300ms max 5s)
+const waitForStore = new Promise((resolve) => {
+  let waited = 0;
+  const maxWait = 5000;
+  const interval = setInterval(() => {
+    if (storeRnf.length > 0 || waited >= maxWait) {
+      clearInterval(interval);
+      resolve();
+    }
+    waited += 300;
+  }, 300);
+});
 
-//   setTimeout(() => {
-//     loader.classList.add("hidden");
-//   }, 500);
-// });
+// Quand carte + données sont prêtes, lancer l’animation du loader
+Promise.all([mapReady, waitForStore]).then(() => {
+  requestAnimationFrame(() => {
+    loader.classList.add("opacity-0");
+    loader.classList.remove("opacity-100");
+
+    setTimeout(() => {
+      loader.classList.add("hidden");
+    }, 400); // animation plus courte
+  });
+});
