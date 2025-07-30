a81c512c0c4ddef3f2dacbf68ea448366f8451cc
PierreNouhen <pierre.nouhen@orange.fr>
2025-07-30 16:37:19 +0200

    Amélioration de l'accessibilité et de generateLoader

--- src/loader/generateLoader.js
+++ src/loader/generateLoader.js
@@ -1,14 +1,23 @@
-const loader = document.getElementById("loader");
+import { generateMap } from "../maps/generateMap";
+import {storeRnf} from "../datas/storeRnf"
 
-function generateLoader() {
-  setTimeout(() => {
-    loader.classList.add("opacity-0");
-    loader.classList.remove("opacity-100");
-  }, 1500);
+// Wait until the storeRnf array is not empty
+const waitForStore = new Promise((resolve) => {
+  const interval = setInterval(() => {
+    // Check if storeRnf has data
+    if (storeRnf.length > 0) {
+      clearInterval(interval);
+      resolve();
+    }
+  }, 100); 
+});
+
+// Wait for both the map and the store to be ready
+Promise.all([generateMap(), waitForStore]).then(() => {
+  loader.classList.add("opacity-0");
+  loader.classList.remove("opacity-100");
 
   setTimeout(() => {
     loader.classList.add("hidden");
-  }, 1800);
-}
-
-generateLoader();
+  }, 500);
+});
