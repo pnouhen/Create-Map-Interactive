7dca7e5abb6f2b6cb10ca2b0a1115613dbdaaeb7
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-12-07 08:09:45 +0100

    fix: delete console.log

--- src/datas/errorData.js
+++ src/datas/errorData.js
@@ -2,7 +2,6 @@ export function errorData() {
   const loader = document.getElementById("loader");
   const message = loader.querySelector("p");
   const points = loader.querySelectorAll("div");
-  console.log(message);
 
   message.textContent = "Le serveur ne répond pas pour le moment.";
   points.forEach((point) => point.remove());
