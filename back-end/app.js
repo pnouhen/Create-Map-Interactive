93c4f56789901bf04325b8485e4829cda53d7a7e
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-12-08 14:51:46 +0100

    feat: create server and database

---
+++ back-end/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+
+console.log(process.env.MONGO_URI);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((err) => console.log("Connexion à MongoDB échouée !", err));
+
+// CORS managment
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+module.exports = app;
+ 
